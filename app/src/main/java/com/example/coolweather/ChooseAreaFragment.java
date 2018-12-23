df254f0f2adfc705d0fef6b45fdc5ff2b8a968c5
lym <374700175@qq.com>
2018-12-23 11:38:10 +0800

    second commit

---
+++ app/src/main/java/com/example/coolweather/ChooseAreaFragment.java
@@ -0,0 +1,24 @@
+package com.example.coolweather;
+
+import android.app.ProgressDialog;
+import android.support.v4.app.Fragment;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChooseAreaFragment extends Fragment {
+    public static final int LEVEL_PROVINCE = 0;
+    public static final int LEVEL_CITY = 1;
+    public static final int LEVEL_COUNTY = 2;
+    private ProgressDialog progressDialog;
+    private TextView titleText;
+    private Button backButton;
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+    private List<String> dataList = new ArrayList<>();
+
+}
